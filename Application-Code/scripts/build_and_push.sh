c883f506efae6493d1f8258a1ea2e2afb7606689
Nomad <nomad@storm.com>
2024-08-25 15:53:14 +0530

    edited script

--- Application-Code/scripts/build_and_push.sh
+++ Application-Code/scripts/build_and_push.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+# Set the path to the Dockerfile and the build context directory
+DOCKERFILE_PATH="Application-Code/backend/Dockerfile"
+BUILD_CONTEXT="Application-Code/backend"
+
 # Authenticate to DockerHub
 echo $DOCKERHUB_PASSWORD | docker login -u $DOCKERHUB_USERNAME --password-stdin
 
 # Build the Docker image
-docker build -t $DOCKER_IMAGE_NAME:$BUILD_NUMBER .
+docker build -t $DOCKERHUB_USERNAME/$DOCKER_IMAGE_NAME:$BUILD_NUMBER -f $DOCKERFILE_PATH $BUILD_CONTEXT
 
 # Push the image to DockerHub
 docker push $DOCKERHUB_USERNAME/$DOCKER_IMAGE_NAME:$BUILD_NUMBER
